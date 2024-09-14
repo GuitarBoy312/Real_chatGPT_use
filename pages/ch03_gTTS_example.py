db80c6211a2a405008341051ab608a3514719436
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-15 00:28:58 +0900

    Update ch03_gTTS_example.py

--- pages/ch03_gTTS_example.py
+++ pages/ch03_gTTS_example.py
@@ -8,11 +8,11 @@ tts = gTTS(text=text, lang="ko")
 tts.save("output.mp3")
 
 # 파일이 제대로 생성되었는지 확인
-if os.path.exists("output.mp3"):
-    st.success("파일이 성공적으로 생성되었습니다.")
+#if os.path.exists("output.mp3"):
+    #st.success("파일이 성공적으로 생성되었습니다.")
     # 스트림릿에서 오디오 파일 재생
-    audio_file = open("output.mp3", "rb")
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format="audio/mp3")
-else:
-    st.error("파일 생성에 실패했습니다.")
+    #audio_file = open("output.mp3", "rb")
+    #audio_bytes = audio_file.read()
+    #st.audio(audio_bytes, format="audio/mp3")
+#else:
+    #st.error("파일 생성에 실패했습니다.")
