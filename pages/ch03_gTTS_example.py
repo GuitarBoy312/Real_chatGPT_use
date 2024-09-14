7850562677076b02c4a6f099144cf493ad5ce1ae
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-15 00:27:17 +0900

    Update ch03_gTTS_example.py

--- pages/ch03_gTTS_example.py
+++ pages/ch03_gTTS_example.py
@@ -1,11 +1,18 @@
+import streamlit as st
 from gtts import gTTS
 import os
 
-tts = gTTS(text="안녕하세요 음성비서 프로그램 실습중입니다.",lang="ko")
+# 텍스트를 음성으로 변환
+text = "안녕하세요 음성비서 프로그램 실습중입니다."
+tts = gTTS(text=text, lang="ko")
 tts.save("output.mp3")
 
 # 파일이 제대로 생성되었는지 확인
 if os.path.exists("output.mp3"):
-    print("파일이 성공적으로 생성되었습니다.")
+    st.success("파일이 성공적으로 생성되었습니다.")
+    # 스트림릿에서 오디오 파일 재생
+    audio_file = open("output.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format="audio/mp3")
 else:
-    print("파일 생성에 실패했습니다.")
+    st.error("파일 생성에 실패했습니다.")
